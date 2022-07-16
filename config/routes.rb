eed40d08469365620ddb08c72a99e170d705ea8e
Simon <simon@7lieues.io>
2022-07-16 17:31:18 +0200

    feat(users): adds basic test view to check if Active Storage is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get '/user/:id' => 'user#show', as: :user_profile
   root 'site#home'
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
